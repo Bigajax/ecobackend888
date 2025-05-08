d2567d724aa86710811d4f5cfe61dbb5ae53eec6
Bigajax <rafaelr.barbon@gmail.com>
2025-05-08 15:39:04 -0300

    Update ProtectedRoute.tsx

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 interface ProtectedRouteProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { user } = useAuth();
-  
-  if (!user) {
-    return <Navigate to="/login" />;
-  }
-  
-  return <>{children}</>;
+    const { user } = useAuth();
+    const navigate = useNavigate(); // Utilize useNavigate para redirecionar
+
+    if (!user) {
+        return <Navigate to="/login" replace />; // Use Navigate e adicione replace
+    }
+
+    return <>{children}</>;
 };
 
 export default ProtectedRoute;
