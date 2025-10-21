a16f9a0642a679209737c1f794a5af6e9d85ec49
Bigajax <rafaelr.barbon@gmail.com>
2025-10-21 10:40:36 -0300

    Fix jest config type import error

---
+++ jest.contract.config.ts
@@ -0,0 +1,17 @@
+const config = {
+  preset: 'ts-jest/presets/default-esm',
+  useESM: true,
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
+  extensionsToTreatAsEsm: ['.ts'],
+  passWithNoTests: true,
+  transformIgnorePatterns: ['/node_modules/'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      tsconfig: '<rootDir>/__tests__/tsconfig.json'
+    }
+  }
+};
+
+export default config;
